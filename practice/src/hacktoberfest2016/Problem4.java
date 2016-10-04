2bb36bb134a6da5900628dfd714bdf6205c97662
dbeta <dwalker.beta@gmail.com>
2016-10-04 19:24:35 -0400

    updates the fourth problem to contain an issue

--- practice/src/hacktoberfest2016/Problem4.java
+++ practice/src/hacktoberfest2016/Problem4.java
@@ -15,7 +15,14 @@ public class Problem4 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		
+		int[] nums = {1,2,3,4,5,6,7,8,9};
+		int sum = 0;
+		
+		for(int i = 0; i < nums.length; i++){
+			sum = nums[i];
+		}
+		System.out.println(sum); //should return 45
 	}
 
 }
