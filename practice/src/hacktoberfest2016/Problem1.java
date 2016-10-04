39a9906e97edff5f857aaa92dddf81b344ee202e
dbeta <dwalker.beta@gmail.com>
2016-10-04 15:08:57 -0400

    This commit contains the example Problem1 with an unsolved issue.

--- practice/src/hacktoberfest2016/Problem1.java
+++ practice/src/hacktoberfest2016/Problem1.java
@@ -8,13 +8,43 @@ package hacktoberfest2016;
 /**
  * 
  */
+//TODO Solve the problem!
 public class Problem1 {
+
+	public static boolean isPalindrome(int x) {
+
+		if (x < 0) {
+			return false;
+		}
+
+		int div = 1;
+		while (x / div >= 10) {
+			div *= 10;
+		}
+		while (x != 0) {
+			int l = x / div;
+			int r = x / 10;
+
+			if (l != r) {
+				return false;
+			}
+			x = (x % div) / 10;
+			div /= 100;
+		}
+		return true;
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		/*These are your test cases. There is nothing wrong with them.*/
+		System.out.println(isPalindrome(0)); //should be true
+		System.out.println(isPalindrome(23)); //should be false
+		System.out.println(isPalindrome(777)); //should be true
+		System.out.println(isPalindrome(1001)); //should be true
+		System.out.println(isPalindrome(2001)); //should be false
 
 	}
 
