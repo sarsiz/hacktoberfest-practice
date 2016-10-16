c8a6d7efaa76965b6d71c7425246d45e7c287c88
sarsiz <sarsiz97@gmail.com>
2016-10-16 15:53:43 +0530

    Problem 6

---
+++ practice/src/hacktoberfest2016/Problem6.java
@@ -0,0 +1,21 @@
+/**
+ * @author Sarsiz Chauhan
+ * Oct 16, 2016
+ * 3:43:21 PM
+ */
+package hacktoberfest2016;
+
+/**
+ * 
+ */
+public class Problem6 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
